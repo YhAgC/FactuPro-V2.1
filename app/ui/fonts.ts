4c4ca53486e6aee73576b292ba542f94b563289a
YhAgC <ixcsgm@gmail.com>
2024-02-20 14:56:16 -0500

    Initial commit

---
+++ app/ui/fonts.ts
@@ -0,0 +1,7 @@
+import { Inter, Montserrat } from 'next/font/google';
+import {  } from 'next/font/google';
+
+export const inter = Inter({ subsets: ['latin'] });
+export const monts = Montserrat ({
+    weight: ['400','500', '700'],
+    subsets: ['latin']});
